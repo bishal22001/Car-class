22ac519d75067d9aa0ac676eb0afc551647c3557
bishal22001 <114582041+bishal22001@users.noreply.github.com>
2022-12-13 17:25:46 +0200

    Add files via upload

---
+++ Car/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1 = new Car();
+		Car car2 = new Car("Huyndai","i1",50);
+		car2.accelerate();
+		car2.accelerate();
+		car2.brake();
+		car2.printData();
+		car2.refuel(4);
+		
+		
+		}
+
+	
+}
